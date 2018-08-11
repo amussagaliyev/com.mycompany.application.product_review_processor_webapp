5fc4d271afda34aee1a5366870a0ee07dc5ff5dd
amussagaliyev <arman.mussagaliyev@gmail.com>
2018-08-11 12:20:22 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/application/product_review/config/ApplicationInitializer.java
@@ -0,0 +1,24 @@
+package com.mycompany.application.product_review.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+	@Override
+	protected Class<?>[] getRootConfigClasses()
+	{
+		return new Class[] { LocalApplicationConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses()
+	{
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings()
+	{
+		return new String[] {};
+	}
+}
